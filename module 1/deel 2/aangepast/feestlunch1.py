f8ecbad7f8036ebd81fada9d5c64d33dcac8aabf
Okamisparx <Natalkanatiiiii11@wp.pl>
2023-11-10 13:39:31 +0100

    .

--- module 1/deel 2/aangepast/feestlunch1.py
+++ module 1/deel 2/aangepast/feestlunch1.py
@@ -14,12 +14,12 @@ korting = aantal_kortingsbonnen * WAARDE_KORTINGSBON
 te_betalen_bedrag = totale_kosten_zonder_korting - korting
 
 kosten_croissantjes /= 100
-kosten_croissantjes /= 100
+kosten_stokbroden /= 100
 te_betalen_bedrag /= 100
 
 print(f"Kosten croissantjes: {kosten_croissantjes} euro")
 print(f"Kosten stokbroden: {kosten_stokbroden} euro")
 print(f"Totale kosten zonder korting: {totale_kosten_zonder_korting} euro")
-print(f"Korting: {korting} cent")
+print(f"Korting: {korting} euro")
 print(f"Te betalen bedrag: {te_betalen_bedrag} euro")
 print(f'De feestlunch kost je bij de bakker {te_betalen_bedrag} euro voor de {aantal_croissantjes} croissantjes en de {aantal_stokbroden} stokbroden als de {aantal_kortingsbonnen} kortingsbonnen nog geldig zijn!')
