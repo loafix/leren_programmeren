2301aa9b4a2cf40ca31013e8de26250ac37e1b8a
Okamisparx <Natalkanatiiiii11@wp.pl>
2023-11-16 11:26:36 +0100

    min en max

---
+++ module 1/deel 3/meten is weten/meten_is_weten4.py
@@ -0,0 +1,16 @@
+a = int(input("eerste gehele getal: "))
+b = int(input("tweede gehele getal: "))
+
+if a > b:
+    Max, Min = a, b
+    print(f'a is het grootste getal: {Max}')
+
+elif a < b:
+    Max, Min = b, a
+    print(f'b is het grootste getal: {Max}')
+
+else:
+    print (f'a en b zijn gelijk')
+
+print(f'Het minimum is: {Min}')
+print(f'Het maximum is: {Max}')
