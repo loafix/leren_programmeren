5b1f97ea56d3d4b3da379d1f23fbb5637a409d8d
Okamisparx <Natalkanatiiiii11@wp.pl>
2023-11-16 11:02:55 +0100

    .

---
+++ module 1/deel 3/meten_is_weten.py
@@ -0,0 +1,9 @@
+a = int(input("eerste gehele getal: "))
+b = int(input("tweede gehele getal: "))
+
+if a > b:
+    Max = a
+    print(f'a is het grootste getal: {Max}')
+else:
+    Max = b
+    print(f'b is het grootste getal: {Max}')
