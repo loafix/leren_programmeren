bc2ce6a82dfcdab0ef41cc1fcebbb6d96a6e471e
Okamisparx <Natalkanatiiiii11@wp.pl>
2023-11-16 11:20:09 +0100

    elif-statement

--- module 1/deel 3/meten_is_weten3.py
+++ module 1/deel 3/meten_is_weten3.py
@@ -4,7 +4,10 @@ b = int(input("tweede gehele getal: "))
 if a > b:
     Max, Min = a, b
     print(f'a is het grootste getal: {Max}')
-    
+
 elif a < b:
     Max, Min = b, a
     print(f'b is het grootste getal: {Max}')
+
+else:
+    print (f'a en b zijn gelijk')
